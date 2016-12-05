48dc50fd957da1eb6dbab395c94648bcf359b32a
EmmaSofie <emmo15ad@student.cbs.dk>
2016-12-05 14:05:19 +0100

    Det er nu også muligt at logge ud, både for brugere og admin.

---
+++ js/logout.js
@@ -0,0 +1,33 @@
+/**
+ * Created by EmmaSofie on 05/12/2016.
+ */
+
+function logout(){
+   //Ingen variable da der ikke skal bruges noget identifikation for at logge ud
+
+    $.ajax({
+        type: "POST",
+        url: "https://localhost:8000/logout",
+        dataType: "json",
+        xhrFields: { withCredentials: true },
+
+        success: function(data) { alert("Success");
+
+            console.log("Aber");
+        window.location.replace("index.html");
+
+            alert(JSON.stringify(data));
+            console.log(JSON.stringify(data));
+
+        },
+
+
+        error: function(data) { alert("Failure");
+            console.log("DUM ");
+
+            alert(JSON.stringify(data));
+
+            console.log(JSON.stringify(data));
+        } // prøv at kør det her prøv igen
+    });
+}
