fdcaff0076424b59588337f9e1fab58d1736ce48
EmmaSofie <emmo15ad@student.cbs.dk>
2016-12-05 11:03:46 +0100

    Jeg er startet med at lave login-funktionen

--- js/login.js
+++ js/login.js
@@ -1,3 +1,25 @@
 /**
  * Created by EmmaSofie on 14/11/2016.
  */
+
+
+function login(){
+    var username = $("#username").val();
+    var password = $("#password").val();
+
+    $.ajax({
+        type: "POST",
+        url: "https://localhost:8000/login",
+        dataType: "json",
+        xhrFields: { withCredentials: true },
+        data: JSON.stringify({
+            "username" : username,
+            "password" : password
+        }),
+
+        success: function(data) { alert("Success");
+
+        },
+        error: function(data) { alert("Failure"); alert(JSON.stringify(data)) }
+    });
+}
